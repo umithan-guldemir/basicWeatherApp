53a891919d85aca6789515abb06f62f90ce34a1e
Ümithan Güldemir <guldemirumithan@gmail.com>
2022-07-18 21:06:35 +0300

    model created

---
+++ basicWeatherApp/Models/Weather.swift
@@ -0,0 +1,20 @@
+//
+//  Weather.swift
+//  basicWeatherApp
+//
+//  Created by Umithan  on 18.07.2022.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    let temperature: String?
+    let wind: String?
+    let forecast: Forecast?
+}
+
+struct Forecast: Codable {
+    let day: String?
+    let temperature: String?
+    let wind: String?
+}
